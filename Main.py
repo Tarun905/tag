918e997c7cbfab41bf897aa706c719b7c2c0323e
BuildTools <unconfigured@null.spigotmc.org>
2019-07-25 11:46:55 -0700

    final project

--- Main.py
+++ Main.py
@@ -1,64 +1,72 @@
-from classes import*
+from classes import Box, Objective, Player
+import sys, pygame
+import random
 
-import pygame
 pygame.init()
-
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((600,600))
 player = Player()
+counter = 0
+timer_sec = 0
+timer_frame = 0
+timer_print = pygame.font.SysFont("Times New Roman", 24)
 objectives = []
-objectives.append(Objective())
-objectives.append(Objective())
-objectives.append(Objective())
-player.box.rect.center = ((500,300))
-objectives[0].box.rect.center = (400,600)
+for i in range(50):
+    objectives.append(Objective())
+player.box.rect.center = ((20,240))
 
-screen = pygame.display.set_mode((1024,720))
+for i in objectives:
+    i.box.rect.center = (random.randint(0,575),random.randint(0,575))
 
 def update():
-    keys = pygame.key.get_pressed()
-    if keys[23]:
+    pygame.event.pump()
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
         player.move(False)
-    if keys[24]:
+    elif key[pygame.K_RIGHT]:
         player.move(True)
-        for i in objectives:
-            i.handleCollision(player.box.rect)
+
+def update2():
+    global counter
+    pygame.event.pump()
+    key = pygame.key.get_pressed()
+    if key[pygame.K_DOWN]:
+        player.movie(True)
+    elif key[pygame.K_UP]:
+        player.movie(False)
+    for i in objectives:
+        if i.handleCollision(player.box.rect):
+            counter += 1
+
+def update3():
+    global timer_frame, timer_sec
+    if counter < 50:
+        timer_frame += 1
+        if timer_frame >= 60:
+            timer_sec += 1
+            timer_frame = 0
+
+
+
 
 def render():
-    screen.fill(pygame.Color("blue"))
+    screen.fill(color=pygame.Color("blue"))
     screen.fill(player.box.color,player.box.rect)
     for i in objectives:
         screen.fill(i.box.color, i.box.rect)
+    screen.blit(timer_print.render((str(timer_sec)), 1, (255, 255, 0, 255)), (0, 0))
+
 
 def main():
     while True:
+        clock.tick(60)
         update()
+        update2()
+        update3()
         render()
         pygame.display.flip()
 if __name__=="__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- classes.py
+++ classes.py
@@ -3,30 +3,45 @@ import pygame
 class Box:
 
     def __init__ (self):
-        self.rect = pygame.Rect(50,50,50,50)
-        self.color = pygame.Color(0,0,0,100)
+        self.rect = pygame.Rect(20,20,20,20)
+        self.color = pygame.Color(0,0,0)
 
     def colorChange(self):
         self.color = pygame.Color(255,0,0,100)
 
 class Player:
 
+
     def __init__(self):
         self.box = Box()
 
+
     def move(self, dir):
         if dir is True:
-            self.box.rect.move_ip(2,0)
+            self.box.rect.move_ip(5,0)
         else:
-            self.box.rect.move_ip(-2,0)
+            self.box.rect.move_ip(-5,0)
+
+    def movie(self, dire):
+        if dire is True:
+            self.box.rect.move_ip(0,5)
+        else:
+            self.box.rect.move_ip(0,-5)
+
 
 class Objective:
     def __init__(self):
         self.box = Box()
+        self.box.color = pygame.Color(255,255,255)
+        self.changed = False
 
     def handleCollision(self,rect):
         if self.box.rect.colliderect(rect):
-            self.box.changeColor()
+            self.box.colorChange()
+            ret = not self.changed
+            self.changed = True
+            return ret
+        return False
 
 
 
