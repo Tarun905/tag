d95f342c695a5071369e61d69e73d8f756e15a53
BuildTools <unconfigured@null.spigotmc.org>
2019-07-26 11:34:49 -0700

    final project 2.0

--- Main.py
+++ Main.py
@@ -7,9 +7,11 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((600,600))
 player = Player()
 counter = 0
-timer_sec = 0
+timer_sec = 25
 timer_frame = 0
 timer_print = pygame.font.SysFont("Times New Roman", 24)
+Game_over_print = pygame.font.SysFont("Times New Roman", 50)
+Winner_print = pygame.font.SysFont("Times New Roman", 50)
 objectives = []
 for i in range(50):
     objectives.append(Objective())
@@ -39,23 +41,31 @@ def update2():
             counter += 1
 
 def update3():
-    global timer_frame, timer_sec
+    global timer_frame, timer_sec, Game_over, Winner
     if counter < 50:
         timer_frame += 1
         if timer_frame >= 60:
-            timer_sec += 1
+            timer_sec -= 1
             timer_frame = 0
-
-
-
+            if timer_sec <= 0:
+                Game_over = Game_over_print.render("Game Over", 1, pygame.Color("White"))
+                screen.blit(Game_over, pygame.Rect(175, 275, 100, 50))
+                pygame.display.flip()
+                pygame.time.delay(900000)
+                pygame.quit()
+            if counter >= 50:
+                Winner = Winner_print.render("Winner", 1, pygame.Color("White"))
+                screen.blit(Winner,pygame.Rect(175,275,100,50))
+                pygame.display.flip()
+                pygame.time.delay(900000)
+                pygame.quit()
 
 def render():
-    screen.fill(color=pygame.Color(77, 77, 77))
+    screen.fill(color = pygame.Color(77,77,77))
     screen.fill(player.box.color,player.box.rect)
     for i in objectives:
         screen.fill(i.box.color, i.box.rect)
     screen.blit(timer_print.render((str(timer_sec)), 1, (255, 255, 0, 255)), (0, 0))
-
 
 def main():
     while True:
@@ -67,16 +77,4 @@ def main():
         pygame.display.flip()
 if __name__=="__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
 
